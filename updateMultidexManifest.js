040c51187044ff245638eadd2e3523278ee0cb96
Nikolay Galko <galek@users.noreply.github.com>
2021-09-25 07:41:16 +0300

    Amend

--- updateMultidexManifest.js
+++ updateMultidexManifest.js
@@ -1,7 +1,7 @@
 module.exports = function(ctx) {
-    var fs = ctx.require('fs'),
-        path = ctx.require('path'),
-        xml = ctx.require('cordova-common').xmlHelpers;
+    var fs = require('fs'),
+        path = require('path'),
+        xml = require('cordova-common').xmlHelpers;
 
     var manifestSubPaths = ['platforms/android/AndroidManifest.xml', 'platforms/android/app/src/main/AndroidManifest.xml'];
     var manifestPath = null;
